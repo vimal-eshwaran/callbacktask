818c8359f0f26fcd57ee4f908aa238ccd3bd7adf
vimal-eshwaran <118733978+vimal-eshwaran@users.noreply.github.com>
2023-02-13 18:17:36 +0530

    Add files via upload

---
+++ callback.js
@@ -0,0 +1,32 @@
+
+setTimeout((content) => {
+    console.log(10)
+   setTimeout((content) => { 
+    console.log(9)
+    setTimeout((content) => {
+    console.log(8)
+    setTimeout((content) => {
+    console.log(7)
+    setTimeout((content) => {
+    console.log(6)
+    setTimeout((content) => {
+    console.log(5)
+    setTimeout((content) => {
+    console.log(4)
+    setTimeout((content) => {
+    console.log(3)
+    setTimeout((content) => {
+    console.log(2)
+    setTimeout((content) => {
+    console.log(1)
+    console.log('happy independence day')
+   },1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000);
